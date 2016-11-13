07d9fafabc905f56682f416a2c54ccef937ef0ad
briandridge <briandridge@gmail.com>
2016-11-13 16:07:47 -0700

    makes it work, moving squares like a boss

--- js/app.js
+++ js/app.js
@@ -1,93 +1,61 @@
 // console.log("Linked!");
 
 var player1div = document.getElementById("player1div");
-console.log(player1);
 var player2div = document.getElementById("player2div");
-console.log(player2);
 var counter1 = 0;
 var counter2 = 0;
-
+x = 10;
 document.onkeydown = checkKey;
+// var percentToMove1 = (counter1 * 10);
+// var percentToMove2 = (counter2 * 10);
 
-function checkKey(e) {
 
-    e = e || window.event;
-
-    if (e.keyCode == '39') {
-        // right arrow - player 1
-        console.log("checked right arrow!");
-        counter1 ++;
-        player1div.setAttribute("class", "clicked");
-    }
-    else if (e.keyCode == '70') {
-        // "f" player 2
-        console.log("checked f key!");
-        counter2 ++;
-        player2div.setAttribute("class", "clicked");
-    }
+// first to 10 keystrokes (100% margin-left)
+// reset counters and margins
+function checkWin() {
+	if (counter1 === 100) {
+		alert("Player 1 wins!");
+		counter1 = 0;
+		counter2 = 0;
+		player1.style.marginLeft = "10px";
+		player2.style.marginLeft = "10px";
+	} else if (counter2 === 100) {
+		alert("Player 2 wins!");
+		counter1 = 0;
+		counter2 = 0;
+		player1.style.marginLeft = "10px";
+		player2.style.marginLeft = "10px";
+	}
 }
 
+// listen to keys, 
+// increment key counter, 
+// add 10% margin-left to corresponding div, 
+// check for win (margin-left = 100%). 
+function checkKey(e) {
 
-// var movePlayer1 = function(){
+    page = e || window.event;
 
-// };
-
-// var movePlayer2 = function(){
-
-// };
-
-// var clickNumber = 0;
-
-// var checkWin1 = funcion(){
-
-// };
-
-// var checkWin2 = function(){
-
-// };
-
-    
-// 	var clickNumber=0;  
-
-// 	$('#board').find('td').on('click', function(){
-// 	    if (clickNumber %2 === 0) {
-// 	      this.innerHTML = "x";
-// 	      this.setAttribute("class", "xClick");
-
-// 	      // checkForWin('x');
-// 	    	} else {
-// 	      this.innerHTML = "o";
-// 	      this.setAttribute("class", "oClick");
-// 	      // checkForWin('o');
-// 	   	}
-// 	    clickNumber ++;
-// 	    console.log(clickNumber);
-// 	 });
-	
-// 	$('#reset').find('button').on('click', function() {
-// 		$('td').removeClass('xClick oClick').addClass('blankState').html("");
-// 		clickNumber = 0;
-// 	});
-
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (page.keyCode === 39) {
+        // right arrow - player 1
+        // console.log("checked right arrow!");
+        counter1 +=10;
+        console.log("player 1 has moved " + counter1 + " times");
+        player1.style.marginLeft = "+" + counter1 + "%";
+		console.log("added " + counter1 + " % margin");
+		checkWin();
+    }
+    else if (page.keyCode === 70) {
+        // "f" player 2
+        // console.log("checked f key!");
+        counter2 +=10;
+        console.log("player 2 has moved " + counter2 + " times");
+        player2div.setAttribute("class", "clicked");
+        player2.style.marginLeft = "+" + counter2 + "%";
+        console.log("added " + counter2 + " % margin");
+        checkWin();
+    }
+}
 
 // ok // Your game should have two pages: a landing page and a game page
 // ok // Include separate HTML, CSS, JavaScript files
@@ -95,8 +63,8 @@ function checkKey(e) {
 // ok // Style your landing page and game page with class-based CSS
 // ok but need to test // Include a media query for a tablet to make your pages responsive
 
-// // Your game page must have at least two player objects
-// // Your game must have a clear win condition
+// ok // Your game page must have at least two player objects
+// ok // Your game must have a clear win condition
 
 // // Comment your code appropriately
 // // Adhere to the AirBnB style guide for writing your JavaScript
